2ba6433314103803e55c014bb6284c489d8e2040
Jacek Obst <jacek.obst@merito.pl>
2023-12-06 16:26:05 +0100

    Create StoreUserTest

---
+++ tests/Feature/User/StoreUserTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\User;
+
+use Tests\Helpers\SanctumTrait;
+use Tests\TestCase;
+
+class StoreUserTest extends TestCase
+{
+    use SanctumTrait;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->actAsAdmin();
+    }
+
+    public function testStoreUserSuccessfully()
+    {
+        // when
+        $response = $this->postJson('/api/users', [
+            'name' => 'John Doe',
+            'email' => 'johndoe@gmail.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        // then
+        $response->assertSuccessful();
+        $this->assertDatabaseHas('users', [
+            'name' => 'John Doe',
+            'email' => 'johndoe@gmail.com',
+        ]);
+    }
+}
