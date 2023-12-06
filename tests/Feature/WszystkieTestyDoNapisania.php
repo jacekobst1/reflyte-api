116f4096bfa723ea346c2d9f753d1f3862d107a1
Jacek Obst <jacek.obst@merito.pl>
2023-12-06 14:40:38 +0100

    no message

---
+++ tests/Feature/WszystkieTestyDoNapisania.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Feature;
+
+final class WszystkieTestyDoNapisania
+{
+    public function user()
+    {
+        /*
+         * 1. Tworzenie usera
+         * 2. Sprawdzanie czy utworzony user ma rolę usera
+         * 3. Sprawdzanie czy hasło zostało zahashowane w db
+         *
+         */
+    }
+}
