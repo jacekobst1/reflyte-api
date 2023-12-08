20d67d36d3db9ea493171afe57060a2b83644890
Jacek Obst <jacek.obst@merito.pl>
2023-12-08 15:26:12 +0100

    Create MakeRequestTrait

--- app/Modules/ESP/Integration/MailerLiteClient.php
+++ app/Modules/ESP/Integration/MailerLiteClient.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 namespace App\Modules\ESP\Integration;
 
-use Illuminate\Support\Facades\Http;
-
 final class MailerLiteClient implements ClientInterface
 {
-    public function __construct(private readonly string $apiKey)
+    use MakeRequestTrait;
+
+    public function __construct(protected readonly string $apiKey)
     {
     }
 
     public function apiKeyIsValid(): bool
     {
-        $response = Http::withToken($this->apiKey)->get('https://connect.mailerlite.com/api/groups?page=1000');
+        $response = $this->makeRequest()->get('https://connect.mailerlite.com/api/groups?page=1000');
 
         return $response->successful();
     }
---
+++ app/Modules/ESP/Integration/MakeRequestTrait.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\ESP\Integration;
+
+use Illuminate\Http\Client\PendingRequest;
+use Illuminate\Support\Facades\Http;
+
+trait MakeRequestTrait
+{
+    private function makeRequest(): PendingRequest
+    {
+        return Http::acceptJson()->withToken($this->apiKey);
+    }
+}
