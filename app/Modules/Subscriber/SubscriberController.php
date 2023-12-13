48b5cd9ec0a3bb5fe7c68c3e96ea4d96c00e85cc
Jacek Obst <jacek.obst@merito.pl>
2023-12-13 18:28:47 +0100

    Move method from CorsFreeController to SubscriberController

--- app/Modules/Subscriber/SubscriberController.php
+++ app/Modules/Subscriber/SubscriberController.php
@@ -10,6 +10,7 @@ use Illuminate\Contracts\Foundation\Application as ApplicationContract;
 use Illuminate\Foundation\Application;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Redirector;
 
 class SubscriberController extends Controller
@@ -24,5 +25,31 @@ class SubscriberController extends Controller
         }
 
         return redirect($subscriber->newsletter->landing_url);
+    }
+
+    // TODO move method logic to separate service
+    public function storeNewSubscriberFromLanding(Request $request): JsonResponse
+    {
+        $request->validate([
+            'email' => ['required', 'email'],
+            'ref_code' => ['required', 'string', 'alpha_num', 'lowercase', 'size:10'],
+        ]);
+
+        $email = $request->input('email');
+        $refCode = $request->input('ref_code');
+
+        $referer = Subscriber::whereRefCode($refCode)->first();
+
+        if (!$referer) {
+            return JsonResp::badRequest('Invalid ref code');
+        }
+
+        Subscriber::create([
+            'referer_subscriber_id' => $referer->id,
+            'newsletter_id' => $referer->newsletter_id,
+            'email' => $email,
+        ]);
+
+        return JsonResp::created();
     }
 }
--- routes/api-cors-free.php
+++ routes/api-cors-free.php
@@ -3,11 +3,12 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\CorsFreeController;
+use App\Modules\Subscriber\SubscriberController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/api')->group(function () {
     Route::prefix('/subscribers')->group(function () {
-        Route::post('/from-landing', [CorsFreeController::class, 'storeNewSubscriberFromLanding']);
+        Route::post('/from-landing', [SubscriberController::class, 'storeNewSubscriberFromLanding']);
     });
 });
 
