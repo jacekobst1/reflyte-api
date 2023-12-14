86c01b67aaa5da741be1d36aa2fd9b64998ee8d0
Jacek Obst <jacek.obst@merito.pl>
2023-12-14 18:25:54 +0100

    Add app_url and api_url variables to env.php config file

--- config/env.php
+++ config/env.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 use Illuminate\Support\Str;
 
 return [
+    'app_url' => env('APP_URL', 'http://reflyte.local'),
+    'api_url' => env('API_URL', 'http://reflyte.local/api'),
     'spa_url' => env('SPA_URL', 'http://app.reflyte.local:3000'),
     'test_user_password' => env('TEST_USER_PASSWORD', Str::random()),
 ];
