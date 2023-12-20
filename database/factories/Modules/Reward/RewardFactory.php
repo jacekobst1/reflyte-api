87b48650578f69b8136682102d39582e830b7cb8
Jacek Obst <jacek.obst@merito.pl>
2023-12-20 19:30:39 +0100

    Create RewardFactory

---
+++ database/factories/Modules/Reward/RewardFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories\Modules\Reward;
+
+use App\Modules\Reward\Reward;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Reward>
+ */
+class RewardFactory extends Factory
+{
+    protected $model = Reward::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'description' => fake()->text(),
+            'required_points' => rand(1, 100),
+        ];
+    }
+}
