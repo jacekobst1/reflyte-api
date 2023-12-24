9b5e43005984b0034f98269c04c2d02b4556812a
Jacek Obst <jacek.obst@merito.pl>
2023-12-24 11:01:37 +0100

    Create WebhookEventTest testWhenSubscriberAlreadyExists()

--- tests/Feature/Esp/WebhookEventTest.php
+++ tests/Feature/Esp/WebhookEventTest.php
@@ -2,19 +2,21 @@
 
 declare(strict_types=1);
 
-namespace Feature\Esp;
+namespace Tests\Feature\Esp;
 
 use App\Modules\Esp\Dto\EspSubscriberStatus;
 use App\Modules\Esp\Integration\EspClientFactory;
 use App\Modules\Esp\Integration\MailerLite\MailerLiteClient;
 use App\Modules\Newsletter\Newsletter;
+use App\Modules\Subscriber\Subscriber;
+use App\Modules\Subscriber\SubscriberIsRef;
 use App\Modules\Subscriber\SubscriberStatus;
 use Illuminate\Support\Str;
 use Tests\TestCase;
 
 final class WebhookEventTest extends TestCase
 {
-    public function testSuccess(): void
+    public function testWhenSubscriberNotExists(): void
     {
         // given
         $newsletter = Newsletter::factory()->create();
@@ -43,7 +45,41 @@ final class WebhookEventTest extends TestCase
             'newsletter_id' => $newsletter->id,
             'email' => $data['email'],
             'status' => SubscriberStatus::Active,
+            'is_ref' => SubscriberIsRef::No,
         ]);
+    }
+
+    public function testWhenSubscriberAlreadyExists()
+    {
+        $email = Str::random() . '@test.com';
+        $newsletter = Newsletter::factory()->create();
+        $subscriber = Subscriber::factory()->for($newsletter)->create([
+            'email' => $email,
+            'status' => SubscriberStatus::Received,
+            'is_ref' => SubscriberIsRef::Yes,
+        ]);
+        $data = [
+            'id' => '123',
+            'email' => $email,
+            'status' => EspSubscriberStatus::Active->value,
+        ];
+
+        // mock
+        $mailerLiteEspClientMock = $this->mock(MailerLiteClient::class);
+        $mailerLiteEspClientMock->shouldReceive('updateSubscriberFields')
+            ->once()
+            ->andReturn(true);
+        $espClientFactoryMock = $this->mock(EspClientFactory::class);
+        $espClientFactoryMock->shouldReceive('make')
+            ->once()
+            ->andReturn($mailerLiteEspClientMock);
+
+        // when
+        $response = $this->post("/api/esp/webhook/$newsletter->id", $data);
+
+        // then
+        $response->assertOk();
+        $this->assertEquals(SubscriberStatus::Active, $subscriber->refresh()->status);
     }
 
     public function testWrongUuid(): void
