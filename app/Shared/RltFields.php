1758778d0c993d02505022e85030a9ff834a5805
Jacek Obst <jacek.obst@merito.pl>
2023-12-24 13:32:46 +0100

    Change RltFields rlt_ref_count -> rlt_referrer_subscriber_id

--- app/Shared/RltFields.php
+++ app/Shared/RltFields.php
@@ -20,23 +20,31 @@ final class RltFields
                 'type' => 'text',
             ],
             [
-                'key' => 'rlt_is_ref',
+                'key' => 'rlt_is_referral',
                 'type' => 'text',
             ],
             [
-                'key' => 'rlt_ref_count',
-                'type' => 'number',
+                'key' => 'rlt_referrer_subscriber_id',
+                'type' => 'text',
             ]
         ];
     }
 
+    /**
+     * @return array{
+     *     rlt_ref_code: string,
+     *     rlt_ref_link: string,
+     *     rlt_is_referral: string,
+     *     rlt_referrer_id: string
+     * }
+     */
     public static function getSubscriberFields(Subscriber $subscriber): array
     {
         return [
             'rlt_ref_code' => $subscriber->ref_code,
             'rlt_ref_link' => $subscriber->ref_link,
-            'rlt_is_ref' => $subscriber->is_ref,
-            'rlt_ref_count' => $subscriber->ref_count,
+            'rlt_is_referral' => $subscriber->is_referral->value,
+            'rlt_referrer_id' => $subscriber->referer_subscriber_id?->toString(),
         ];
     }
 }
