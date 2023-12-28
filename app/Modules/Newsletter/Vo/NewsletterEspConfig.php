ae2f9a2631bcc4fd86cfcb51fb65e6be4262b774
Jacek Obst <jacek.obst@merito.pl>
2023-12-28 15:45:38 +0100

    no message

--- app/Modules/Newsletter/Vo/NewsletterEspConfig.php
+++ app/Modules/Newsletter/Vo/NewsletterEspConfig.php
@@ -7,13 +7,12 @@ namespace App\Modules\Newsletter\Vo;
 use App\Modules\Esp\EspName;
 use Ramsey\Uuid\UuidInterface;
 
-// TODO make every classes like this readonly
-final class NewsletterEspConfig
+final readonly class NewsletterEspConfig
 {
     public function __construct(
-        public readonly UuidInterface $newsletterId,
-        public readonly EspName $espName,
-        public readonly string $espApiKey,
+        public UuidInterface $newsletterId,
+        public EspName $espName,
+        public string $espApiKey,
     ) {
     }
 }
