1f9fee1a2dc0690c36cfd4baf216ff522da8bbed
Jacek Obst <jacek.obst@merito.pl>
2024-01-28 10:22:48 +0100

    Allow only admin to view Telescope on production

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -60,7 +60,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     {
         Gate::define('viewTelescope', function ($user) {
             return in_array($user->email, [
-                //
+                'jacek@reflyte.com'
             ]);
         });
     }
