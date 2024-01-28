df0dbc688c36e9af602deb80cb83b1c1bfedef62
Jacek Obst <jacek.obst@merito.pl>
2024-01-28 11:05:08 +0100

    Install Laravel Pulse

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -7,7 +7,9 @@ namespace App\Providers;
 // use Illuminate\Support\Facades\Gate;
 use App\Modules\Reward\Reward;
 use App\Modules\Reward\RewardPolicy;
+use App\Modules\User\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +27,10 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('viewPulse', function (User $user) {
+            return in_array($user->email, [
+                'jacek@reflyte.com',
+            ]);
+        });
     }
 }
